12c0db79a858435dc117a4ad60b019e7439397b9
Drew Radcliff <sirampr@gmail.com>
2020-08-20 12:27:13 -0400

    Add addpost form

--- ghostpost/urls.py
+++ ghostpost/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from homepage import views
+
 urlpatterns = [
+    path('', views.index, name="homepage"),
+    path('addpost', views.add_post),
     path('admin/', admin.site.urls),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
 
-# Create your views here.
+from datetime import datetime
+
+from homepage.models import Post
+from homepage.forms import AddPostForm
+
+
+def index(request):
+    posts = Post.objects.all()
+    return render(request, "index.html", {"posts": posts})
+
+
+def add_post(request):
+    if request.method == "POST":
+        form = AddPostForm(request.POST)
+        form.save()
+        # if form.is_valid():
+        #     data = form.cleaned_data
+        #     new_post = Post.objects.create(
+        #         is_boast=data.get('is_boast'),
+        #         post_text=data.get('post_text'),
+        #         up_votes=0,
+        #         down_votes=0,
+        #         submission_date=
+        #     )
+        return HttpResponseRedirect(reverse("homepage"))
+
+    form = AddPostForm()
+    return render(request, "generic_form.html", {"form": form})
