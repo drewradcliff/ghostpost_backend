a31c04f30aeb56faf7b9dd3f8465ac4c4826dfca
Drew Radcliff <sirampr@gmail.com>
2020-09-29 12:41:54 -0400

    Add extra actions for upvote/downvote

--- homepage/views.py
+++ homepage/views.py
@@ -3,6 +3,8 @@ from homepage.models import Post
 from homepage.serializers import PostSerializer
 
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -11,3 +13,19 @@ class PostViewSet(viewsets.ModelViewSet):
     """
     queryset = Post.objects.all().order_by('-submission_date')
     serializer_class = PostSerializer
+
+    @action(detail=True, methods=['post'])
+    def add_upvote(self, request, pk=None):
+        post = self.get_object()
+        post.up_votes += 1
+        post.score = post.up_votes - post.down_votes
+        post.save()
+        return Response({'status': 'post upvoted'})
+
+    @action(detail=True, methods=['post'])
+    def add_downvote(self, request, pk=None):
+        post = self.get_object()
+        post.down_votes += 1
+        post.score = post.up_votes - post.down_votes
+        post.save()
+        return Response({'status': 'post downvoted'})
